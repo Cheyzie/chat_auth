e5678b3075dfcd1f39c0771a13811b84a780fd4a
cheyzie <pyroducktail@gmail.com>
2025-02-14 01:20:58 +0200

    feat: added id to user response

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	ID       uint   `json:"-" db:"id"`
+	ID       uint   `json:"id" db:"id"`
 	Username string `json:"username" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password,omitempty" binding:"required"`
